7bee20948abcb03e7823e97fd29e2fd24b17af9f
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-08-05 12:22:05 -0300

    maxodds

--- src/features/constants/superOddsProviders.js
+++ src/features/constants/superOddsProviders.js
@@ -1,14 +1,15 @@
-// src/constants/superOddsProviders.js (COMPLETO E ATUALIZADO)
+// src/constants/superOddsProviders.js (ATUALIZADO)
 const SUPER_ODDS_AFFILIATED_PROVIDERS = {
     'bet365': { name: 'Bet365', link: 'https://www.bet365.bet.br/' }, // Seu link afiliado Bet365
-    'superbet': { name: 'Superbet', link: 'https://bdeal.io/Superbet/110998/1' },
-    'mc_games': { name: 'McGames', link: 'https://bdeal.io/mcgames/125292/1' }, // Note que a API CraqueStats usa 'mc_games'
+    'superbet': { name: 'Superbet', link: 'https://afiliapub.scaletrk.com/click?o=11&a=550218488' },
+    'mc_games': { name: 'McGames', link: 'https://go.aff.mcgames.bet/fedczzag' }, // Note que a API CraqueStats usa 'mc_games'
     'betano': { name: 'Betano', link: 'https://bdeal.io/Betano/124683/1' },
     'betfair': { name: 'Betfair', link: 'https://bdeal.io/Betfair/61765/1' },
-    'kto': { name: 'KTO', link: 'https://bdeal.io/kto/127471/1' },
+    'kto': { name: 'KTO', link: 'https://afiliapub.scaletrk.com/click?o=28&a=550218488' },
     'novibet': { name: 'Novibet', link: 'https://bdeal.io/novibet/111009/1' }, // Verifique novamente este link para Novibet, pois é o mesmo da Superbet
     'betmgm': { name: 'BetMGM', link: 'https://bdeal.io/betmgm/123274/1' },
     'betsson': { name: 'Betsson', link: 'https://bdeal.io/Betsson/127093/1' },
+    'stake': { name: 'Stake', link: 'https://afiliapub.scaletrk.com/click?o=82&a=550218488' },
 };
 
 module.exports = SUPER_ODDS_AFFILIATED_PROVIDERS;
