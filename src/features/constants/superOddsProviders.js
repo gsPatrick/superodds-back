7f4aad9df9e481b627f8e5be158b5078c8b39441
Patrick Siqueira <118585848+gsPatrick@users.noreply.github.com>
2025-08-13 16:22:20 -0300

    Update superOddsProviders.js

--- src/features/constants/superOddsProviders.js
+++ src/features/constants/superOddsProviders.js
@@ -1,16 +1,17 @@
-// src/constants/superOddsProviders.js (COMPLETO E ATUALIZADO)
+// src/constants/superOddsProviders.js (COMPLETO E CORRIGIDO)
 const SUPER_ODDS_AFFILIATED_PROVIDERS = {
-    'bet365': { name: 'Bet365', link: 'https://www.bet365.bet.br/' }, // Seu link afiliado Bet365
+    'bet365': { name: 'Bet365', link: 'https://www.bet365.bet.br/' },
     'superbet': { name: 'Superbet', link: 'https://bdeal.io/Superbet/110998/1' },
-    'mc_games': { name: 'McGames', link: 'https://bdeal.io/mcgames/125292/1' }, // Note que a API CraqueStats usa 'mc_games'
+    'mc_games': { name: 'McGames', link: 'https://bdeal.io/mcgames/125292/1' },
     'betano': { name: 'Betano', link: 'https://bdeal.io/Betano/124683/1' },
     'betfair': { name: 'Betfair', link: 'https://bdeal.io/Betfair/61765/1' },
     'kto': { name: 'KTO', link: 'https://bdeal.io/kto/127471/1' },
-    'novibet': { name: 'Novibet', link: 'https://bdeal.io/novibet/111009/1' }, // Verifique novamente este link para Novibet, pois é o mesmo da Superbet
+    'novibet': { name: 'Novibet', link: 'https://bdeal.io/novibet/111009/1' },
     'betmgm': { name: 'BetMGM', link: 'https://bdeal.io/betmgm/123274/1' },
     'betsson': { name: 'Betsson', link: 'https://bdeal.io/Betsson/127093/1' },
-    'bateu_bet': { name: 'BateuBet', link: 'https://bdeal.io/BateuBet/119087/1' },
-    'esportiva_bet': { name: 'BateuBet', link: 'https://bdeal.io/EsportivaBet/128422/1' },
+    'bateubet': { name: 'BateuBet', link: 'https://bdeal.io/BateuBet/119087/1' }, // CORRIGIDO: de 'bateu_bet' para 'bateubet'
+    'esportiva': { name: 'EsportivaBet', link: 'https://bdeal.io/EsportivaBet/128422/1' }, // CORRIGIDO: de 'esportiva_bet' para 'esportiva' e nome ajustado
+    'marjo': { name: 'MarjoSports', link: 'https://go.affiliapass.com?id=686d1206d7b5de00154ce5f4' } // ADICIONADO: Provedor faltante. Substitua pelo seu link de afiliado.
 };
 
 module.exports = SUPER_ODDS_AFFILIATED_PROVIDERS;
